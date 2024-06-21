ebe582210fb8df6909a301921178574f0c72fabe
jkemming <git@jkemming.com>
2024-06-21 20:49:02 +0200

    refactor: Split `config.fish` into multiple files in `conf.d`

---
+++ .config/fish/conf.d/1_starship.fish
@@ -0,0 +1,22 @@
+# Customize character used in transient prompt history
+function starship_transient_prompt_func
+  starship module character
+end
+
+starship init fish | source
+enable_transience
+
+# Fix issues with transient prompt
+## Only execute command if it isn't empty. Prevents issue where multiple prompts aren't cleared when hitting `enter`
+## without a command entered.
+function transient_execute_if_not_empty
+  set -l command (commandline)
+  if test -n "$command"
+    transient_execute
+  end
+end
+bind \r 'transient_execute_if_not_empty'
+bind \n 'transient_execute_if_not_empty'
+## Make `ctrl+c` clear the input. Prevents issue where the input along with the prompt wouldn't be cleared and instead
+## a new prompt created
+bind \cc 'commandline ""'
---
+++ .config/fish/conf.d/2_abbreviations.fish
@@ -0,0 +1,31 @@
+abbr -a g git
+abbr -a ga git add
+abbr -a gaa git add --all
+abbr -a gb git branch
+abbr -a gbr git branch --remote
+abbr -a gc git commit
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gds git diff --staged
+abbr -a gf git fetch
+abbr -a gl git pull
+abbr -a gp git push
+abbr -a gst git status
+abbr -a gsw git switch
+abbr -a l ls
+abbr -a tf terraform
+abbr -a tfa terraform apply
+abbr -a tfi terraform init
+abbr -a tfp terraform plan
+
+# Allow going up multiple directories by typing multiple dots
+function multicd
+  echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+abbr --add multicd --regex '^\.\.+$' --function multicd
+
+# Allow repeating the last command with `!!`
+function last_history_item
+    echo $history[1]
+end
+abbr -a !! --position anywhere --function last_history_item
