8d8ca7b67d0f58e00e72187ccd2c8bc574d7832a
jkemming <git@jkemming.com>
2024-06-21 20:53:05 +0200

    docs: Add explanation for config overrides

--- .config/fish/conf.d/2_config_overrides.fish
+++ .config/fish/conf.d/2_config_overrides.fish
@@ -1,1 +1,3 @@
+# Intentionally not set as universal variables (`set -U`) since that introduces persistent, untracked changes which
+# hurt reproducability on new systems where those changes haven't been applied.
 set -g fish_greeting ''
