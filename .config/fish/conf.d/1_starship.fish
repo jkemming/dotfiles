40a4fd0b93ede2b95ee04fe31a626bd8ac5acb16
jkemming <git@jkemming.com>
2024-06-23 15:14:52 +0200

    fix: Repaint on `ctrl+c` to redraw prompt on terminal resize

--- .config/fish/conf.d/1_starship.fish
+++ .config/fish/conf.d/1_starship.fish
@@ -19,4 +19,8 @@ bind \r 'transient_execute_if_not_empty'
 bind \n 'transient_execute_if_not_empty'
 ## Make `ctrl+c` clear the input. Prevents issue where the input along with the prompt wouldn't be cleared and instead
 ## a new prompt created
-bind \cc 'commandline ""'
+function cancel_and_repaint
+  commandline ""
+  commandline -f repaint
+end
+bind \cc 'cancel_and_repaint'
