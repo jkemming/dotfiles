251c0611fca2e4ef1803f2437d3e00671b070959
jkemming <git@jkemming.com>
2024-08-15 18:56:40 +0200

    feat: Add abbreviation for git fetch and rebase

--- .config/fish/conf.d/2_abbreviations.fish
+++ .config/fish/conf.d/2_abbreviations.fish
@@ -1,22 +1,23 @@
-abbr -a g git
-abbr -a ga git add
-abbr -a gaa git add --all
-abbr -a gb git branch
-abbr -a gbr git branch --remote
-abbr -a gc git commit
-abbr -a gco git checkout
-abbr -a gd git diff
-abbr -a gds git diff --staged
-abbr -a gf git fetch
-abbr -a gl git pull
-abbr -a gp git push
-abbr -a gst git status
-abbr -a gsw git switch
-abbr -a l ls
-abbr -a tf terraform
-abbr -a tfa terraform apply
-abbr -a tfi terraform init
-abbr -a tfp terraform plan
+abbr -a g 'git'
+abbr -a ga 'git add'
+abbr -a gaa 'git add --all'
+abbr -a gb 'git branch'
+abbr -a gbr 'git branch --remote'
+abbr -a gc 'git commit'
+abbr -a gco 'git checkout'
+abbr -a gd 'git diff'
+abbr -a gds 'git diff --staged'
+abbr -a gf 'git fetch'
+abbr -a gfr 'git fetch && git rebase'
+abbr -a gl 'git pull'
+abbr -a gp 'git push'
+abbr -a gst 'git status'
+abbr -a gsw 'git switch'
+abbr -a l 'ls'
+abbr -a tf 'terraform'
+abbr -a tfa 'terraform apply'
+abbr -a tfi 'terraform init'
+abbr -a tfp 'terraform plan'
 
 # Allow going up multiple directories by typing multiple dots
 function multicd
