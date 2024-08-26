74ac0f58d242c2450d4d6e8c9955f4fb84add8eb
jkemming <git@jkemming.com>
2024-08-26 22:42:32 +0200

    feat: Add keexp config

--- .config/fish/functions/keexp.fish
+++ .config/fish/functions/keexp.fish
@@ -1,3 +1,3 @@
 function keexp
-	command keexp "$HOME/Nextcloud/KeePass/Credentials.kdbx" "$HOME/Nextcloud/keexp_config.json" | source
+	command keexp "$HOME/Nextcloud/KeePass/Credentials.kdbx" "$HOME/.config/keexp.json" | source
 end
