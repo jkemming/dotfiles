684edca5b314e27529e548e939709941c91d4e88
jkemming <git@jkemming.com>
2024-11-10 18:26:34 +0100

    feat: Auto-supply sudo password

--- .config/fish/functions/keexp.fish
+++ .config/fish/functions/keexp.fish
@@ -1,3 +1,6 @@
 function keexp
 	command keexp "$HOME/Nextcloud/KeePass/Credentials.kdbx" "$HOME/.config/keexp.json" | source
+	function sudo
+		SUDO_ASKPASS="$HOME/.config/echo_user_password" command sudo -A $argv
+	end
 end
