63133173814ca78cc1cf3ec53ea332aca6447ad2
jkemming <git@jkemming.com>
2026-05-03 14:34:29 +0200

    chore: update to fish 4.3

--- .config/fish/conf.d/fish.fish
+++ .config/fish/conf.d/fish.fish
@@ -1,3 +1,5 @@
 # Intentionally not set as universal variables (`set -U`) since that introduces persistent, untracked changes which
 # hurt reproducability on new systems where those changes haven't been applied.
 set -g fish_greeting ''
+
+fish_config theme choose default-rgb
