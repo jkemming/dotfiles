b86de502dbaad1eec6a2e0586b5ef8f0aeac87fa
jkemming <git@jkemming.com>
2026-08-30 16:17:11 +0200

    feat: add private mode indicator to tab title

--- .config/fish/conf.d/fish_title.fish
+++ .config/fish/conf.d/fish_title.fish
@@ -1,6 +1,9 @@
 functions -c fish_title __original_fish_title
 
 function fish_title
+    if set -q fish_private_mode
+        echo -n "🕶️ "
+    end
     if set -q __shell_title_override
         echo $__shell_title_override
     else
