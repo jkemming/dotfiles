824e1da75a31faabc6e5ffd2f350e0d33038616c
jkemming <git@jkemming.com>
2026-09-01 22:25:31 +0200

    fix: remove broken transient prompt customizations
    No longer works since https://github.com/starship/starship/commit/cc65e0c7429068f1123fe5d9844ebebdfa1b9547 and there seems to be no easy way to implement the same functionality. Also, this breaks newline handling when typing in multiple commands while one is already running.

--- .config/fish/conf.d/starship.fish
+++ .config/fish/conf.d/starship.fish
@@ -5,20 +5,3 @@ end
 
 starship init fish | source
 enable_transience
-
-# Override default `transient_execute` function to tweak the following:
-#   - Prevent multiple prompts in case of invalid fish syntax (e.g. `echo "${test}"`)
-#   - Prevent empty commands being executed by simply pressing `enter`
-function transient_execute
-    set -l command (commandline)
-    if test -z "$command"
-        return
-    end
-
-    set -g TRANSIENT 1
-    set -g RIGHT_TRANSIENT 1
-    commandline -f repaint
-    commandline -f execute
-end
-# Prevent `ctrl-j` from printing multiple prompts
-bind ctrl-j 'transient_execute'
